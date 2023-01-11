d181d02eb02d20ce3ba4cd813e7bcfca460575dc
KlepovAlexander <sania.klepov@gmail.com>
2023-01-11 23:26:18 +0300

    Remove trash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/screens/location_screen.dart';
 
-// import 'screens/weather_forecast_screen.dart';
 
 void main() {
   runApp(const MyApp());
